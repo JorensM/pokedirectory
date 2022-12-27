e63dcf97f981bb1efe6b2892051becb22b2e95f1
JorensM <jorensmerenjanu@gmail.com>
2022-12-27 11:23:55 +0200

    part 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request,Response,Application} from 'express';
+import { request } from 'http';
 
 const app:Application = express();
 
@@ -6,6 +7,10 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/", (req:Request, res:Response):void => {
     res.send("Hello from server!")
+});
+
+app.get("/test", (req:Request, res:Response):void => {
+    res.json("Hello to client from server!");
 });
 
 app.listen(PORT, ():void => {
