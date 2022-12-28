60d1843f6ffa748b547b832d0fb40345008868e0
JorensM <jorensmerenjanu@gmail.com>
2022-12-28 12:19:34 +0200

    part 4

---
+++ client/src/components/NavOption/NavOption.tsx
@@ -0,0 +1,17 @@
+//client/src/components/NavOption/NavOption.css
+
+//Core
+import { useState, useEffect } from "react";
+
+//Style
+import "./NavOption.css";
+
+
+//Props type
+type NavOptionProps = {
+    color: string
+}
+
+export default function NavOption(props: NavOptionProps){
+
+}
