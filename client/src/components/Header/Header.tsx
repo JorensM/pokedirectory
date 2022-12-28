3ea4873093986b139eeccac6768dfd2e7901a61c
JorensM <jorensmerenjanu@gmail.com>
2022-12-28 10:54:57 +0200

    part 4

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+//client/src/components/Header/Header.tsx
+
+//Style
+import "./Header.css";
+
+export default function Header(){
+
+    return (
+        <div className="Header">
+            <div className="HeaderLeft">
+                PokeDirectory
+            </div>
+            <div className="HeaderRight">
+                Nav
+            </div>
+        </div>
+    )
+}
---
+++ client/src/components/Page/Page.tsx
@@ -0,0 +1,18 @@
+//client/src/components/Page/Page.tsx
+
+//Style
+import "./Page.css";
+
+export default function Page(){
+
+    return (
+        <div className="Page">
+            <div className="PageTop">
+                Pagetop
+            </div>
+            <div className="PageBottom">
+                Pagebot
+            </div>
+        </div>
+    )
+}
