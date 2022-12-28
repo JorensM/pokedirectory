a36210037f2d08fc209cc8f55bbb1d8ed7f7421c
JorensM <jorensmerenjanu@gmail.com>
2022-12-28 09:59:19 +0200

    part 3

--- server/src/endpoints/loginUser.ts
+++ server/src/endpoints/loginUser.ts
@@ -7,25 +7,38 @@ import { Database } from "sqlite3";
 //Functions
 import validateUser from "../fn/validateUser";
 
+//Endpoint for logging in the user
 export default function loginUser(app: Application, db: Database){
+    //Add endpoint
     app.post("/loginUser", (req: Request, res: Response):void => {
 
+        //Get username and password from request body
         const username = req.body.username;
         const password = req.body.password;
 
+        //Validate username and password combo
         validateUser(db, username, password)
         .then(valid => {
+
+            //Check if combo is valid
             if(valid){
+                //If valid, set session variable "username" to the user's username
                 req.session.username = username;
+                //Return true as response
                 res.json(true);
             }else{
+                //If not valid, set session variable "username" to undefined
                 req.session.username = undefined;
+                //Return false as response
                 res.json(false);
             }
         })
+        //Error handling
         .catch(err => {
-            console.error("Error logging in");
+            console.error("Error logging in: ");
             console.error(err);
+
+            //Return false as response
             res.json(false);
         }) 
     });
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,16 +8,23 @@ import session from "express-session";
 //Functions
 import initEndpoints from './endpoints';
 
+
+//Type definitions for session storage
 declare module 'express-session' {
     interface SessionData {
         username: string;
     }
 }
 
+
+//Create new Express app
 const app:Application = express();
 
+//Ports
 const PORT = process.env.PORT || 8000;
 
+
+//Database initialization
 let db = new Database("db/main.db", (err) => {
     if(err){
         console.error("Failed connecting to database: ");
@@ -28,15 +35,20 @@ let db = new Database("db/main.db", (err) => {
     }
 });
 
+//This function is to support request body parsing.
 app.use(express.json());
+
+//This initializes session storage
 app.use(session({
     resave: true,
     saveUninitialized: true,
     secret: "keyboard cat"
 }));
 
+//This initializes the endpoints
 initEndpoints(app, db);
 
+//Start the server
 app.listen(PORT, ():void => {
     console.log(`Server running @ https://localhost:${PORT}`);
 });
